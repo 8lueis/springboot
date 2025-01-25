db2a6dccc2c344a5de28cd8556c7b43b7196e800
8lue <hkahk@naver.com>
2025-01-25 18:08:24 +0900

    첫 번째 커밋

---
+++ src/main/java/khk/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -0,0 +1,13 @@
+package khk.springbootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 모든 프로젝트에는 메인 클래스가 필요
+@SpringBootApplication
+public class SpringBootDeveloperApplication {
+    public static void main(String[] args){
+        SpringApplication.run(SpringBootDeveloperApplication.class, args);
+    }
+}
+
