5da3d7a19c4035d3e6101285536ace8d17f7af24
8lue <hkahk@naver.com>
2025-01-27 23:42:29 +0900

    프레젠테이션, 비즈니스, 퍼시스턴트 계층 학습

--- src/main/java/khk/springbootdeveloper/TestController.java
+++ src/main/java/khk/springbootdeveloper/TestController.java
@@ -1,16 +1,22 @@
 package khk.springbootdeveloper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+// 프레젠테이션 계층 == 컨트롤러
 @RestController
 public class TestController {
+
+    // TestService 빈 주입
+    @Autowired
+    TestService testService;
+
     @GetMapping("/test")
-    public String test(){
-        return "Hello, world";
+    public List<Member> getAllMembers(){
+        List<Member> members = testService.getAllMembers();
+        return members;
     }
 }
-
-// 메인 클래스 Xx (메인 클래스를 실행해야 localhost로 접속 가능)
-// localhost: 12.0.0.1 == 지금 사용 중인 컴퓨터를 의미
-// '/test' : @GetMapping이라는 애너테이션으로 메서드와 매핑할 때 설정한 경로
